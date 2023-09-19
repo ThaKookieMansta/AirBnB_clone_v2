d1b16f025b031bebad943d0ab01c594da957cd91
Chris Mukirae <chriskagiko@gmail.com>
2023-09-19 21:58:08 +0300

    Phase 1 complete
    Project phase complete, Ready for unittests.

---
+++ models/engine/db_storage.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+"""
+Defines the DBStorage Engine for the project
+"""
+from os import getenv
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+from models.amenity import Amenity
+from models.base_model import Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+class DBStorage:
+    """
+    This Class represents the dbstorage engine
+    """
+    __engine = None
+    __session = None
+
+    def __int__(self):
+        """
+        Initializes the DBStorage instances
+        """
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
+                                      format(getenv("HBNB_MYSQL_USER"),
+                                             getenv("HBNB_MYSQL_PWD"),
+                                             getenv("HBNB_MYSQL_HOST"),
+                                             getenv("HBNB_MYSQL_DB")),
+                                      pool_pre_ping=True)
+        if getenv("HBNB_ENV") == "test":
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """Query on the curret database session all objects of the given class.
+
+        If cls is None, queries all types of objects.
+
+        Return:
+            Dict of queried classes in the format <class name>.<obj id> = obj.
+        """
+        if cls is None:
+            objs = self.__session.query(State).all()
+            objs.extend(self.__session.query(City).all())
+            objs.extend(self.__session.query(User).all())
+            objs.extend(self.__session.query(Place).all())
+            objs.extend(self.__session.query(Review).all())
+            objs.extend(self.__session.query(Amenity).all())
+        else:
+            if type(cls) == str:
+                cls = eval(cls)
+            objs = self.__session.query(cls)
+        return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
+
+    def new(self, obj):
+        """Add obj to the current database session."""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commit all changes to the current database session."""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Delete obj from the current database session."""
+        if obj is not None:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Create all tables in the database and initialize a new session."""
+        Base.metadata.create_all(self.__engine)
+        curr_db_session = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        safe_session = scoped_session(curr_db_session)
+        self.__session = safe_session()
+
+    def close(self):
+        """Close the working SQLAlchemy session."""
+        self.__session.close()
