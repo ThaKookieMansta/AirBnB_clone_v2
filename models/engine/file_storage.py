1667be3943323779ed1ad57b73cd0a081315ca5c
Chris Mukirae <chriskagiko@gmail.com>
2023-09-21 01:07:07 +0300

    Correction files_storage.py Correction on the specific file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,6 +7,12 @@ import json
 import datetime
 
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class FileStorage:
